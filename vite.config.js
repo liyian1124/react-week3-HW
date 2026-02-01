3760b62b00928b1d7ae9312403c359e3da860dc7
Ian Li <yian33514@gmail.com>
2026-02-01 13:44:23 +0800

    修改為 react-week3-HW

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? '/react-wek3-HW/' : '/',
+  base: process.env.NODE_ENV === 'production' ? '/react-week3-HW/' : '/',
   plugins: [react()],
 })
